3f97988a576d33427063738fd0bb943e2d533309
rohitumadi <rohitumadi2000@gmail.com>
2023-12-15 20:58:12 +0530

    added navbar component

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
+function Homepage() {
+  return (
+    <div>
+      <PageNav />
+      <Link to="/pricing">Pricing</Link>
+    </div>
+  );
+}
+
+export default Homepage;
